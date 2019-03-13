c87478166f8018ad7b0819a389d615e461ec1886
Saif Mohammed <saifmohammedbloger@gmail.com>
2019-03-13 11:17:34 +0530

    React Practice Project

---
+++ Components/Home.js
@@ -0,0 +1,16 @@
+var React=require('react');
+var Link=require('react-router-dom').Link;
+
+class Home extends React.Component{
+    render(){
+        return(
+        <div className='home-container'>
+            <h1>Github Battle: battle your frinds andd stff..</h1>
+                <Link className='button' to ='/battle'>
+                    Battle        
+                </Link>
+        </div>
+        )
+    }
+}
+module.exports=Home;
---
+++ index.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App =require('./components/App');
+
+require('./index.css');
+
+ 
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+);
